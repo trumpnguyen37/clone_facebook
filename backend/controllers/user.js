9da544e565acf6af922cd250ef682e8441d96d02
trumpnguyen37 <vantrung03072002@gmail.com>
2023-08-10 22:04:09 +0700

    finish register and send emails

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,4 @@
+const { sendVerificationEmail } = require("../helpers/mailer");
 const { generateToken } = require("../helpers/tokens");
 const {
   validateEmail,
@@ -68,9 +69,19 @@ exports.register = async (req, res) => {
       { id: user._id.toString() },
       "30m"
     );
-    console.log(emailVerificationToken);
-
-    res.json(user);
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerificationEmail(user.email, user.first_name, url);
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success ! please activate your email to start",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
