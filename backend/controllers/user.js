8a43ac4129ccd6a554e496f4b33b72e5afcb7821
trumpnguyen37 <vantrung03072002@gmail.com>
2023-08-10 14:14:57 +0700

    username live validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,8 @@
-const { validateEmail, validateLength } = require("../helpers/validation");
+const {
+  validateEmail,
+  validateLength,
+  validateUsername,
+} = require("../helpers/validation");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 exports.register = async (req, res) => {
@@ -45,14 +49,15 @@ exports.register = async (req, res) => {
     }
 
     const cryptedPassword = await bcrypt.hash(password, 12);
-    console.log(cryptedPassword);
-    return;
+
+    let tempUsername = first_name + last_name;
+    let newUsername = await validateUsername(tempUsername);
     const user = await new User({
       first_name,
       last_name,
       email,
-      password,
-      username,
+      password: cryptedPassword,
+      username: newUsername,
       bYear,
       bMonth,
       bDay,
--- backend/helpers/validation.js
+++ backend/helpers/validation.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 exports.validateEmail = (email) => {
   return String(email)
     .toLowerCase()
@@ -8,4 +10,20 @@ exports.validateLength = (text, min, max) => {
     return false;
   }
   return true;
+};
+
+exports.validateUsername = async (username) => {
+  let a = false;
+
+  do {
+    let check = await User.findOne({ username });
+    if (check) {
+      //change username
+      username += (+new Date() * Math.random()).toString().substring(0, 1);
+      a = true;
+    } else {
+      a = false;
+    }
+  } while (a);
+  return username;
 };
