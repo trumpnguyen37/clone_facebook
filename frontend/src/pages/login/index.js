cadf979b57a5bdd1408e2e7ec00381d799069f4d
trumpnguyen37 <vantrung03072002@gmail.com>
2023-08-12 00:11:01 +0700

    Create UI and Two-way binding give Login and Register

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,3 +1,74 @@
+import "./style.css";
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
+import LoginInput from "../../components/inputs/logininput";
+import { useState } from "react";
+const loginInfos = {
+  email: "",
+  password: "",
+};
 export default function Login() {
-  return <div>Login</div>;
+  const [login, setLogin] = useState(loginInfos);
+  const { email, password } = login;
+  console.log(login);
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="../icons/facebook.svg" alt="" />
+            <span>
+              Facebook helps you connect and share with the people in your life
+            </span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik
+                initialValues={{
+                  email,
+                  password,
+                }}
+              >
+                {(formik) => (
+                  <Form>
+                    <LoginInput
+                      value={email}
+                      type="text"
+                      name="email"
+                      placeholder="Email address or phone number"
+                      onChange={handleLoginChange}
+                    />
+                    <LoginInput
+                      value={password}
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={handleLoginChange}
+                    />
+                    <button type="submit" className="blue_btn">
+                      Log In
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+              <Link to="/forgot" className="forgot_password">
+                Forgotten password ?
+              </Link>
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">Create Account</button>
+            </div>
+            <Link to="/" className="sign_extra">
+              <b>Create a page</b>
+              for a celebrity, brand or business
+            </Link>
+          </div>
+        </div>
+        <div className="register"></div>
+      </div>
+    </div>
+  );
 }
