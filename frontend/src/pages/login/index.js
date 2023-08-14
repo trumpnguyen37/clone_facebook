22a922946be44cec38d4966fe5131f652eccf343
trumpnguyen37 <vantrung03072002@gmail.com>
2023-08-14 12:15:48 +0700

    create ui footer

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import { Formik, Form } from "formik";
 import { Link } from "react-router-dom";
 import * as Yup from "yup";
-import LoginInput from "../../components/inputs/logininput";
+import LoginInput from "../../components/inputs/loginInput";
 import { useState } from "react";
 const loginInfos = {
   email: "",
@@ -27,9 +27,9 @@ export default function Login() {
       <div className="login_wrapper">
         <div className="login_wrap">
           <div className="login_1">
-            <img src="../icons/facebook.svg" alt="" />
+            <img src="../../icons/facebook.svg" alt="" />
             <span>
-              Facebook helps you connect and share with the people in your life
+              Facebook helps you connect and share with the people in your life.
             </span>
           </div>
           <div className="login_2">
@@ -45,14 +45,12 @@ export default function Login() {
                 {(formik) => (
                   <Form>
                     <LoginInput
-                      // value={email}
                       type="text"
                       name="email"
                       placeholder="Email address or phone number"
                       onChange={handleLoginChange}
                     />
                     <LoginInput
-                      // value={password}
                       type="password"
                       name="password"
                       placeholder="Password"
@@ -66,18 +64,74 @@ export default function Login() {
                 )}
               </Formik>
               <Link to="/forgot" className="forgot_password">
-                Forgotten password ?
+                Forgotten password?
               </Link>
               <div className="sign_splitter"></div>
               <button className="blue_btn open_signup">Create Account</button>
             </div>
             <Link to="/" className="sign_extra">
-              <b>Create a page</b>
-              for a celebrity, brand or business
+              <b>Create a Page</b> for a celebrity, brand or business.
             </Link>
           </div>
         </div>
         <div className="register"></div>
+        <footer className="login_footer">
+          <div className="login_footer_wrap">
+            <Link to="/">English(UK)</Link>
+            <Link to="/">Français(FR)</Link>
+            <Link to="/">العربية</Link>
+            <Link to="/">ⵜⴰⵎⴰⵣⵉⵖⵜ</Link>
+            <Link to="/">Español (España)</Link>
+            <Link to="/">italiano</Link>
+            <Link to="/">Deutsch</Link>
+            <Link to="/">Português (Brasil)</Link>
+            <Link to="/">हिन्दी</Link>
+            <Link to="/">中文(简体)</Link>
+            <Link to="/">日本語</Link>
+            <Link to="/" className="footer_square">
+              <i className="plus_icon"></i>
+            </Link>
+          </div>
+          <div className="footer_splitter"></div>
+          <div className="login_footer_wrap">
+            <Link to="/">Sign Up</Link>
+            <Link to="/">Log in</Link>
+            <Link to="/">Messenger</Link>
+            <Link to="/">Facebook Lite</Link>
+            <Link to="/">Watch</Link>
+            <Link to="/">Places</Link>
+            <Link to="/">Games</Link>
+            <Link to="/">Marketplace</Link>
+            <Link to="/">Facebook Pay</Link>
+            <Link to="/">Oculus</Link>
+            <Link to="/">Portal</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Bulletin</Link>
+            <Link to="/">Local</Link>
+            <Link to="/">Fundraisers</Link>
+            <Link to="/">Services</Link>
+            <Link to="/">Voting Information Centre</Link>
+            <Link to="/">Groups</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Create ad</Link>
+            <Link to="/">Create Page</Link>
+            <Link to="/">Developers</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Privacy</Link>
+            <Link to="/">Cookies</Link>
+            <Link to="/">
+              AdChoices
+              <i className="adChoices_icon"></i>
+            </Link>
+            <Link to="/">Terms</Link>
+            <Link to="/">Help</Link>
+          </div>
+          <div className="login_footer_wrap">
+            <Link to="/" style={{ fontSize: "12px", marginTop: "10px" }}>
+              Meta © 2022
+            </Link>
+          </div>
+        </footer>
       </div>
     </div>
   );
