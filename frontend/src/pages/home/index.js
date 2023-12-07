1a3e1001d63936445a1662728e2b48581c92d2e6
Van Trung <99741692+trumpnguyen37@users.noreply.github.com>
2023-12-07 15:01:27 +0700

    left home menu

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 import Header from "../../components/header";
 import useClickOutside from "../../helpers/clickOutside";
+import LeftHome from "../../components/home/left";
+import { useSelector } from "react-redux";
 
 export default function Home() {
   const [visible, setVisible] = useState(true);
@@ -9,9 +11,11 @@ export default function Home() {
     setVisible(false);
     console.log("u clicked outside");
   });
+  const { user } = useSelector((user) => ({ ...user }));
   return (
     <div>
       <Header />
+      <LeftHome user={user} />
       {visible && <div className="card" ref={el}></div>}
     </div>
   );
