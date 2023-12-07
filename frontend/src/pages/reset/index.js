9dfc1c67fbadc527abdb6ec23a527d86adffb835
Van Trung <99741692+trumpnguyen37@users.noreply.github.com>
2023-12-08 03:03:01 +0700

    find user and reset code email FE

--- frontend/src/pages/reset/index.js
+++ frontend/src/pages/reset/index.js
@@ -14,12 +14,14 @@ export default function Reset() {
   const { user } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [visible, setVisible] = useState(3);
+  const [visible, setVisible] = useState(0);
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [code, setCode] = useState("");
   const [password, setPassword] = useState("");
   const [conf_password, setConf_password] = useState("");
   const [error, setError] = useState("");
+  const [userInfos, setUserInfos] = useState("");
   const logout = () => {
     Cookies.set("user", "");
     dispatch({
@@ -27,6 +29,7 @@ export default function Reset() {
     });
     navigate("/login");
   };
+  console.log(userInfos);
   return (
     <div className="reset">
       <div className="reset_header">
@@ -53,9 +56,27 @@ export default function Reset() {
       </div>
       <div className="reset_wrap">
         {visible === 0 && (
-          <SearchAccount email={email} setEmail={setEmail} error={error} />
+          <SearchAccount
+            email={email}
+            setEmail={setEmail}
+            error={error}
+            setError={setError}
+            setLoading={setLoading}
+            setUserInfos={setUserInfos}
+            setVisible={setVisible}
+          />
         )}
-        {visible === 1 && <SendEmail user={user} />}
+        {visible === 1 && userInfos && (
+          <SendEmail
+            email={email}
+            userInfos={userInfos}
+            error={error}
+            setError={setError}
+            setLoading={setLoading}
+            setUserInfos={setUserInfos}
+            setVisible={setVisible}
+          />
+        )}
         {visible === 2 && (
           <CodeVerification
             user={user}
