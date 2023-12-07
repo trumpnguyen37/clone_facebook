de309e217e16859a7f88bae735920aaf7a6d0d2d
Van Trung <99741692+trumpnguyen37@users.noreply.github.com>
2023-12-07 15:40:01 +0700

    Let's work on stories

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -4,6 +4,8 @@ import useClickOutside from "../../helpers/clickOutside";
 import LeftHome from "../../components/home/left";
 import { useSelector } from "react-redux";
 import RightHome from "../../components/home/right";
+import Stories from "../../components/home/stories";
+import "./style.css";
 
 export default function Home() {
   const [visible, setVisible] = useState(true);
@@ -14,9 +16,12 @@ export default function Home() {
   });
   const { user } = useSelector((user) => ({ ...user }));
   return (
-    <div>
+    <div className="home">
       <Header />
       <LeftHome user={user} />
+      <div className="home_middle">
+        <Stories />
+      </div>
       <RightHome user={user} />
       {visible && <div className="card" ref={el}></div>}
     </div>
