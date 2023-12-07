481a56eba26d905679db0fc3e9c47ee9a18d4a70
Van Trung <99741692+trumpnguyen37@users.noreply.github.com>
2023-12-08 02:57:03 +0700

    find user and validate reset code email BE

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,10 +4,12 @@ const {
   validateUsername,
 } = require("../helpers/validation");
 const User = require("../models/User");
+const Code = require("../models/Code");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../helpers/tokens");
-const { sendVerificationEmail } = require("../helpers/mailer");
+const { sendVerificationEmail, sendResetCode } = require("../helpers/mailer");
+const generateCode = require("../helpers/generateCode");
 exports.register = async (req, res) => {
   try {
     const {
@@ -160,6 +162,42 @@ exports.sendVerification = async (req, res) => {
     sendVerificationEmail(user.email, user.first_name, url);
     return res.status(200).json({
       message: "Email verification link has been sent to your email.",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.findUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email }).select("-password");
+    if (!user) {
+      return res.status(400).json({
+        message: "Account does not exists.",
+      });
+    }
+    return res.status(200).json({
+      email: user.email,
+      picture: user.picture,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.sendResetPasswordCode = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email }).select("-password");
+    await Code.findOneAndRemove({ user: user._id });
+    const code = generateCode(5);
+    const savedCode = await new Code({
+      code,
+      user: user._id,
+    }).save();
+    sendResetCode(user.email, user.first_name, code);
+    return res.status(200).json({
+      message: "Email reset code has been sent to your email",
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,9 @@ const {
   activateAccount,
   login,
   auth,
+  sendVerification,
+  findUser,
+  sendResetPasswordCode,
 } = require("../controllers/user");
 const { authUser } = require("../middlwares/auth");
 
@@ -12,5 +15,8 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/activate", authUser, activateAccount);
 router.post("/login", login);
+router.post("/sendVerification", authUser, sendVerification);
+router.post("/findUser", findUser);
+router.post("/sendResetPasswordCode", sendResetPasswordCode);
 
 module.exports = router;
