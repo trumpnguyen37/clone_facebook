0da03f0afd35ced786538f28e3061e8686953695
Van Trung <99741692+trumpnguyen37@users.noreply.github.com>
2023-12-13 01:02:00 +0700

    post reacts and actions fe

--- frontend/src/components/post/index.js
+++ frontend/src/components/post/index.js
@@ -2,7 +2,11 @@ import { Link } from "react-router-dom";
 import "./style.css";
 import Moment from "react-moment";
 import { Dots, Public } from "../../svg";
+import ReactsPopup from "./ReactsPopup";
+import { useState } from "react";
 export default function Post({ post }) {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="post">
       <div className="post_header">
@@ -73,6 +77,43 @@ export default function Post({ post }) {
           )}
         </>
       )}
+      <div className="post_infos">
+        <div className="reacts_count">
+          <div className="reacts_count_imgs"></div>
+          <div className="reacts_count_num"></div>
+        </div>
+        <div className="to_right">
+          <div className="comments_count">13 comments</div>
+          <div className="share_count">1 share</div>
+        </div>
+      </div>
+      <div className="post_actions">
+        <ReactsPopup visible={visible} setVisible={setVisible} />
+        <div
+          className="post_action hover1"
+          onMouseOver={() => {
+            setTimeout(() => {
+              setVisible(true);
+            }, 500);
+          }}
+          onMouseLeave={() => {
+            setTimeout(() => {
+              setVisible(false);
+            }, 500);
+          }}
+        >
+          <i className="like_icon"></i>
+          <span>Like</span>
+        </div>
+        <div className="post_action hover1">
+          <i className="comment_icon"></i>
+          <span>Comment</span>
+        </div>
+        <div className="post_action hover1">
+          <i className="share_icon"></i>
+          <span>Share</span>
+        </div>
+      </div>
     </div>
   );
 }
