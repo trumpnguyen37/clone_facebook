811fd283923128f7e83dbce9cc516c4915b9a9a0
Van Trung <99741692+trumpnguyen37@users.noreply.github.com>
2023-12-22 17:28:22 +0700

    profile header, cover section, picture, infos, menu

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,9 +1,13 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { profileReducer } from "../../functions/reducers";
-
+import Header from "../../components/header";
+import "./style.css";
+import Cover from "./Cover";
+import ProfielPictureInfos from "./ProfielPictureInfos";
+import ProfileMenu from "./ProfileMenu";
 export default function Profile() {
   const { username } = useParams();
   const navigate = useNavigate();
@@ -45,6 +49,16 @@ export default function Profile() {
       });
     }
   };
-  console.log(profile);
-  return <div>Profile</div>;
+  return (
+    <div className="profile">
+      <Header page="profile" />
+      <div className="profile_top">
+        <div className="profile_container">
+          <Cover cover={profile.cover} />
+          <ProfielPictureInfos profile={profile} />
+          <ProfileMenu />
+        </div>
+      </div>
+    </div>
+  );
 }
